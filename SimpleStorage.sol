9d7bce400a381432a00e4e22c8ef9c13f55264d6
Avisake Chowdhury <74789568+avisakechowdhury@users.noreply.github.com>
2022-01-29 14:03:35 +0530

    Update: SimpleStorage.sol
    updated SimpleStorage.sol . Now one can add person and their favourite number in people array and find one's favourite number through mapping nameToFavouriteNumber.
    
    // so far ok

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,14 +1,28 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity >= 0.6.0 <0.9.0 ;
 
-contract SimpleStorage {
 
-    uint number ;
-    function store(uint _storeThis) public{
-        number = _storeThis;
-        
+contract simpleStorage {
+    uint256  favouriteNumber;
+    struct People{
+        string Name;
+        uint256 favouriteNumber;
     }
-    function get() public view returns(uint){
-        return number;
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
     }
-    
+
+    function retrieve() public view returns (uint256){
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name , uint256 _favouriteNumber) public {
+        people.push(People(_name , _favouriteNumber));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
 }
